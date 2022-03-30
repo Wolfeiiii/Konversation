31a2780dc9579939d00a7ea5faa8575ed2de480d
Wolfeiiii <84038372+Wolfeiiii@users.noreply.github.com>
2022-03-30 11:47:34 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,15 @@
 let categories = {"Döda ett barn", "Adoptera ett barn"};
+  
+document.getElementById("button").addEventListener(
+    "click",
+    function() {
+      let project = categories[Math.floor(Math.random() * categories.length)];
+      let link = document.getElementById('content')
+
+      link.innerHTML = project;
+    }
+  );
+
 
 window.onload = function onLoad() {
 	// Store list of entries by category name
