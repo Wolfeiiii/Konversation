f1a214f6a045d229cd1f65e861f93e5aaecf2e26
Wolfeiiii <84038372+Wolfeiiii@users.noreply.github.com>
2022-03-30 11:25:09 +0200

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,39 @@
+let categories = {"Döda ett barn", "Adoptera ett barn"};
+
+window.onload = function onLoad() {
+	// Store list of entries by category name
+	generate();
+}
+
+function generate() {
+  let result = pickRandom(');
+  document.getElementById("content").innerHTML = result;
+}
+
+function pickRandom() {
+	
+	let random_index = Math.floor(Math.random() * categories.length); 
+	
+	// Avoid duplicates:
+	let max_iterations = 5;
+	for (let i = 0; i < max_iterations; i ++) {
+		var result = categories[random_index];
+		if (recently_used.includes(result)) {
+			random_index = (random_index + 1) % category.length;
+		}
+		else {
+			recently_used.push(result);
+			break;
+		}
+	}
+	return result;
+}
+
+function pickRandomFromList(list) {
+	let random_index = Math.floor(Math.random() * list.length); 
+	return list[random_index];
+}
+
+function randomChance(probability) {
+	return Math.random() < probability;
+}
